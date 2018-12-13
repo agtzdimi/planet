5fde96b50b2f7d709b6a7e88abad822d3bc3ec54
Dimitris Agtzidis <agtzdimi@gmail.com>
2018-12-13 17:55:38 +0200

    Web app and File Upload API

---
+++ simulationUI/planet/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "./components/SubmitFile";
+import SubmitFile from "./components/SubmitFile";
+
+class App extends Component {
+  backgroundImgStyle =
+    "body { \
+       background-image: url(planet.jpg); \
+       height: 100%; margin: 0; \
+       background-repeat: no-repeat; \
+       background-size: cover; } \
+    ";
+
+  render() {
+    return (
+      <div>
+        <SubmitFile
+          message="Upload Components and Simulation Parameter File"
+          id="file1"
+        />
+        <SubmitFile message="Upload Energy Load File" id="file2" />
+        <SubmitFile message="Upload Simulink File" id="file3" />
+        <style>{this.backgroundImgStyle}</style>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ simulationUI/planet/src/components/SubmitFile.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import { post } from "axios";
+
+class SubmitFile extends Component {
+  state = {
+    fileName: "",
+    buttonText: "Select File",
+    url: "http://localhost:8000/upload",
+    data: ""
+  };
+
+  onChange(e) {
+    let files = e.target.files;
+    if (files[0] !== undefined) {
+      this.setState({ fileName: files[0].name });
+      this.setState({ buttonText: files[0].name });
+      let reader = new FileReader();
+      reader.readAsDataURL(files[0]);
+
+      reader.onload = () => {
+        const dataFile = new FormData();
+        dataFile.append("file", files[0], files[0].name);
+        this.setState({ data: dataFile });
+      };
+    }
+  }
+
+  handleUpload() {
+    post(this.state.url, this.state.data).then(response =>
+      console.log("result", response)
+    );
+  }
+
+  render() {
+    return (
+      <div onSubmit={this.onFormSubmit}>
+        <h3>{this.props.message}</h3>
+        <input
+          type="file"
+          id={this.props.id}
+          name="myfile"
+          onChange={e => this.onChange(e)}
+          style={{
+            display: "none"
+          }}
+        />
+
+        <label htmlFor={this.props.id} className="btn btn-primary m-2">
+          {this.state.buttonText}
+        </label>
+        <input
+          className="btn btn-primary"
+          name="Submit"
+          id="Submit"
+          type="submit"
+          value="Upload"
+          onClick={() => this.handleUpload()}
+        />
+      </div>
+    );
+  }
+}
+
+export default SubmitFile;
