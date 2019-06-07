fe24534e03a7c38f79e6114c6332e0b4d1ab5312
agtzdimi <agtzdimi@gmail.com>
2019-06-07 10:34:18 +0000

    Replace markLine with markArea to better represent reverse powerflow #40

--- planet/src/app/pages/simulation-run/simulation-start/simulation-area-stack.component.ts
+++ planet/src/app/pages/simulation-run/simulation-start/simulation-area-stack.component.ts
@@ -25,41 +25,43 @@ export class SimulationsAreaStackComponent implements OnDestroy, OnChanges {
     }
 
     setData(type, name, data) {
-        let markLine = null;
+        let markArea = null;
         if (name === 'Electric grid power flow') {
-            markLine = {
+            markArea = {
+                silent: true,
                 data: [
-                    {
-                        yAxis: 0.1,
-                        label: {
-                            show: true,
-                            position: 'middle',
-                            formatter: 'Electricity imported from external grid',
+                    [
+                        {
+                            yAxis: 0,
+                            label: {
+                                show: true,
+                                position: ['50%', '50%'],
+                                formatter: 'Electricity imported from external grid',
+                            },
+                            itemStyle: {
+                                normal: {
+                                    color: 'rgba(231, 76, 60, 1)',
+                                    borderColor: '#CE1C08',
+                                    borderWidth: 1,
+                                    borderType: 'dotted',
+                                },
+                            },
                         },
-                        lineStyle: {
-                            opacity: 0.7,
+                        {
+                            yAxis: 'max',
                         },
-                    },
-                    {
-                        yAxis: -0.1,
-                        label: {
-                            show: true,
-                            position: 'middle',
-                            formatter: 'Reverse power flow',
-                        },
-                        lineStyle: {
-                            opacity: 0.7,
-                        },
-                    },
+                    ],
                 ],
             };
+            name = 'Electricity imported from external grid';
         }
         if (type === 'line') {
             return {
                 name: name,
                 type: 'line',
                 data: data,
-                markLine: markLine,
+                hoverAnimation: false,
+                markArea: markArea,
             };
         } else {
             return {
@@ -114,6 +116,35 @@ export class SimulationsAreaStackComponent implements OnDestroy, OnChanges {
                             csvData[index][val] = parseFloat(csvData[index][val]).toFixed(6);
                         }
                         tempData = this.setData('line', name, csvData[index]);
+                        const gridPowerFlow = { ...tempData };
+                        series.push(gridPowerFlow);
+                        tempData['markArea'] = {
+                            silent: true,
+                            data: [
+                                [
+                                    {
+                                        yAxis: -0,
+                                        label: {
+                                            show: true,
+                                            position: ['50%', '50%'],
+                                            formatter: 'Reverse power flow',
+                                        },
+                                        itemStyle: {
+                                            normal: {
+                                                color: 'rgba(38, 194, 129, 1)',
+                                                borderColor: '#CE1C08',
+                                                borderWidth: 1,
+                                                borderType: 'dotted',
+                                            },
+                                        },
+                                    },
+                                    {
+                                        yAxis: 'min',
+                                    },
+                                ],
+                            ],
+                        };
+                        tempData['name'] = 'Reverse power flow';
                         break;
                     case 'Total_heat_demand':
                         name = 'Total heat demand';
@@ -214,10 +245,10 @@ export class SimulationsAreaStackComponent implements OnDestroy, OnChanges {
                     type: 'inside',
                     show: true,
                     start: 0,
-                    end: 25,
+                    end: 40,
                 }, {
                     start: 0,
-                    end: 25,
+                    end: 40,
                     handleIcon: `M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h
 1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z`,
                     handleSize: '80%',
