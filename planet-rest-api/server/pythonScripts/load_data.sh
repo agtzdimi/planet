89a3d4407fb66956627225520ea32c3e0a7d6d65
agtzdimi <agtzdimi@gmail.com>
2019-10-14 06:08:10 +0000

    Optimize MongoDB queries closes #78

--- planet-rest-api/server/pythonScripts/load_data.sh
+++ planet-rest-api/server/pythonScripts/load_data.sh
@@ -8,21 +8,24 @@ echo "$formName #######################$(pwd)"
 
 mkdir -p "$dirName/loadData"
 mongoexport --port $MONGO_PORT --host $MONGO_IP -u $MONGO_USER -p $MONGO_PASSWORD \
-    --authenticationDatabase $MONGO_AUTH_DB --db planet --collection files --out "$dirName/loadData/allDocuments.txt"
-grep '"formName":"'"$formName"'"' "$dirName/loadData/allDocuments.txt" | grep 'Parameters_initialization' > "$dirName/loadData/Parameters_initialization.txt"
-grep '"formName":"'"$formName"'"' "$dirName/loadData/allDocuments.txt" | grep 'Control_initialization' > "$dirName/loadData/Control_initialization.txt"
-grep '"formName":"'"$formName"'"' "$dirName/loadData/allDocuments.txt" | grep 'Economy_environment_initialization' > "$dirName/loadData/Economy_environment_initialization.txt"
-grep '"formName":"'"$formName"'"' "$dirName/loadData/allDocuments.txt" | grep 'Wind.xlsx' > "$dirName/loadData/WindData.txt"
-grep '"formName":"'"$formName"'"' "$dirName/loadData/allDocuments.txt" | grep 'PV.xlsx' > "$dirName/loadData/PVData.txt"
+    --authenticationDatabase $MONGO_AUTH_DB --db planet --collection files --out "$dirName/loadData/allDocuments.txt" -q '{"payload.formName": "'$formName'"}'
+grep 'Parameters_initialization' "$dirName/loadData/allDocuments.txt" > "$dirName/loadData/Parameters_initialization.txt"
+grep 'Control_initialization' "$dirName/loadData/allDocuments.txt" > "$dirName/loadData/Control_initialization.txt"
+grep 'Economy_environment_initialization' "$dirName/loadData/allDocuments.txt" > "$dirName/loadData/Economy_environment_initialization.txt"
+grep 'Wind.xlsx' "$dirName/loadData/allDocuments.txt" > "$dirName/loadData/WindData.txt"
+grep 'PV.xlsx' "$dirName/loadData/allDocuments.txt" > "$dirName/loadData/PVData.txt"
 mongoexport --port $MONGO_PORT --host $MONGO_IP -u $MONGO_USER -p $MONGO_PASSWORD \
-    --authenticationDatabase $MONGO_AUTH_DB --db planet --collection files --type=csv --fields DH_demand,LH_demand,"formName",Time --out "$dirName/loadData/Heat.csv"
-egrep ','"$formName"',|DH_demand|LH_demand' "$dirName/loadData/Heat.csv" | sed 's|,'"$formName"'||' | sed 's/,formName//' | sed '/^,,.*/d' > "$dirName/tempFileloadData"
+    --authenticationDatabase $MONGO_AUTH_DB --db planet --collection files --type=csv --fields DH_demand,LH_demand,"formName",Time \
+    --out "$dirName/loadData/Heat.csv" -q '{"formName": "'$formName'"}'
+sed 's|,'"$formName"'||' "$dirName/loadData/Heat.csv" | sed 's/,formName//' | sed '/^,,.*/d' > "$dirName/tempFileloadData"
 sortField=$(awk 'BEGIN {FS=OFS=","} {print NF}' "$dirName/tempFileloadData" | head -n1)
 (head -n 1 "$dirName/tempFileloadData" && tail -n +2 "$dirName/tempFileloadData" | sort -n -k$sortField,$sortField -t,) | cut -d , -f1,2 > "$dirName/loadData/Heat.csv"
 
 mongoexport --port $MONGO_PORT --host $MONGO_IP -u $MONGO_USER -p $MONGO_PASSWORD \
-    --authenticationDatabase $MONGO_AUTH_DB --db planet --collection files --type=csv --fields Active_node1,Reactive_node1,Active_node2,Reactive_node2,Active_node3,Reactive_node3,Active_node4,Reactive_node4,Active_node5,Reactive_node5,Active_node6,Reactive_node6,Active_node7,Reactive_node7,Active_node8,Reactive_node8,"formName",Time --out "$dirName/loadData/Electricity.csv"
-egrep ','"$formName"',|Active_node|Reactive_node' "$dirName/loadData/Electricity.csv" | sed 's|,'"$formName"'||' | sed 's/,formName//' | sed '/^,,.*/d' > "$dirName/tempFileloadData"
+    --authenticationDatabase $MONGO_AUTH_DB --db planet --collection files --type=csv \
+    --fields Active_node1,Reactive_node1,Active_node2,Reactive_node2,Active_node3,Reactive_node3,Active_node4,Reactive_node4,Active_node5,Reactive_node5,Active_node6,Reactive_node6,Active_node7,Reactive_node7,Active_node8,Reactive_node8,"formName",Time \
+    --out "$dirName/loadData/Electricity.csv" -q '{"formName": "'$formName'"}'
+sed 's|,'"$formName"'||' "$dirName/loadData/Electricity.csv" | sed 's/,formName//' | sed '/^,,.*/d' > "$dirName/tempFileloadData"
 sortField=$(awk 'BEGIN {FS=OFS=","} {print NF}' "$dirName/tempFileloadData" | head -n1)
 (head -n 1 "$dirName/tempFileloadData" && tail -n +2 "$dirName/tempFileloadData" | sort -n -k$sortField,$sortField -t,) | cut -d, -f1-16 > "$dirName/loadData/Electricity.csv"
 
